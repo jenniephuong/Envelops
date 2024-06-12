3ed0cf61fb41350c4cacd2e19338ff88fc632558
Jennie <Jennie954@outlook.com>
2024-06-13 00:09:21 +0100

    created envelope status bars

--- app/src/main/java/com/example/envelops/MainActivity.kt
+++ app/src/main/java/com/example/envelops/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.envelops
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.envelops.ui.theme.EnvelopsTheme
@@ -26,15 +33,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun EnvelopeScreen(name: String) {
-    var envelopes = ArrayList<String>()
-    envelopes.add("Travel")
-    envelopes.add("Food")
-    envelopes.add("Shopping")
-    envelopes.add("Social")
-    envelopes.add("Investments")
-    envelopes.add("Target")
-
-
+    val rows = 5
+    val cols = 2
+    val envelopes: Array<Array<Any?>> = Array(rows) { Array<Any?>(cols) { null } }
+    envelopes[0][0] = "Transport"
+    envelopes[0][1] = 120.00
+    envelopes[1][0] = "Food"
+    envelopes[1][1] = 55.00
+    envelopes[2][0] = "Shopping"
+    envelopes[2][1] = 7.89
+    envelopes[3][0] = "Investments"
+    envelopes[3][1] = -70.11
+    envelopes[4][0] = "Social"
+    envelopes[4][1] = -12.13
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -43,21 +54,41 @@ fun EnvelopeScreen(name: String) {
         Text(text = "Today the date is...")
         Text(text = "Here are your envelopes")
         for(envelope in envelopes){
-            Envelope(envelope,200.00, 100.00)
+            Envelope(envelope[0].toString(),200.00, envelope[1].toString().toDouble())
         }
     }
 }
 
 @Composable
 fun Envelope(envelopeName: String, budgetAmount: Double, amount: Double) {
-    Row(
+    val colour: Color
+    val progress: Float
+    if (amount >= 0) { // positive progress bar
+        progress = ((budgetAmount - amount) / budgetAmount).toFloat()
+        colour = Color.Green
+    } else { // negative progress bar
+        progress = (Math.abs(amount)  / budgetAmount).toFloat()
+        colour = Color.Red
+    }
+    Column (
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp)
-    ) {
-        Text(text = envelopeName)
-        Text(text = "$amount")
-
+            .padding(15.dp)
+            .background(Color.LightGray)
+    ){
+        Row() {
+            Text(text = envelopeName)
+            Text(text = "$amount")
+            Text(text = "$budgetAmount")
+        }
+        LinearProgressIndicator(
+            progress = progress,
+            color = colour,
+            modifier = Modifier
+                .padding(15.dp)
+                .height(8.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .fillMaxWidth()
+        )
     }
 }
 
@@ -68,5 +99,3 @@ fun EnvelopsPreview() {
         EnvelopeScreen("Squishie")
     }
 }
-
-// dummy commit
